dd4f71419fb9fff187f8dcc39943b0e94cb74636
Hanna Dobrowolska <tak.obiektyw.nie@gmail.com>
2022-06-04 18:38:15 +0200

    add square movement

--- main.js
+++ main.js
@@ -4,14 +4,26 @@ let divY = 50;
 div.style.left = `${divX}px`;
 div.style.top = `${divY}px`;
 
+let drowActive = false ;
+
 div.addEventListener('mousedown' , () => {
     div.style.backgroundColor = "#747d8c"
+    drowActive = true ;
 })
 
-div.addEventListener('mousemove' , () => {
-    console.log("ruch");
+div.addEventListener('mousemove' , (e) => {
+    if (drowActive) {
+
+        divX = e.clientX ;
+        divY = e.clientY ;
+
+        div.style.left = `${divX-100}px`;
+        div.style.top = `${divY-100}px`;
+    }
+
 })
 
 div.addEventListener('mouseup' , () => {
+    drowActive = false ;
     div.style.backgroundColor = "#000"
 })
